1af132ef9443b409efe8b2880c030504f80addf2
Hussainuzzaman-Hannan <mdhussainuzzamanhannan@gmail.com>
2026-07-19 13:19:45 +0600

    modified:AddMaintenanceScreen.kt

--- app/src/main/java/com/example/bikeexpensetracker/ui/theme/screens/AddMaintenanceScreen.kt
+++ app/src/main/java/com/example/bikeexpensetracker/ui/theme/screens/AddMaintenanceScreen.kt
@@ -1,5 +1,6 @@
 package com.example.bikeexpensetracker.ui.screens
 
+import com.example.bikeexpensetracker.data.SelectedBikeManager
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -340,7 +341,7 @@ fun AddMaintenanceScreen(
                                 odometerAtService = odometer.toIntOrNull() ?: 0,
                                 note = note,
                                 category = selectedCategory,
-                                bikeId = 1
+                                bikeId = SelectedBikeManager.getSelectedBikeId()
                             )
                             viewModel.addMaintenanceEntry(maintenanceEntry)
                             onSaveSuccess()
